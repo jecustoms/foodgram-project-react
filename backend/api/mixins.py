f99bfe0ad177efda4d364beddcde35ccb1df2d54
Evgeny Nikiforov <stud.nikiforov@gmail.com>
2021-08-10 15:05:36 +0300

    Mixins fix

--- backend/api/mixins.py
+++ backend/api/mixins.py
@@ -3,7 +3,7 @@ from rest_framework import mixins, viewsets
 from .permissions import IsStaffOrReadOnly
 
 
-class BaseTagAndIngredientViewSet(mixins.ListModelMixinx,
+class BaseTagAndIngredientViewSet(mixins.ListModelMixin,
                                   mixins.RetrieveModelMixin,
                                   viewsets.GenericViewSet):
     permission_classes = [IsStaffOrReadOnly]
